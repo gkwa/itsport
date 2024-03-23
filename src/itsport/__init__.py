e7e4f30b935d1ba01e3600d35b4ede6bea7896ab
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-23 15:25:00 -0700

    Initial untested

--- src/itsport/__init__.py
+++ src/itsport/__init__.py
@@ -1,10 +1,21 @@
+import argparse
 import sys
 
 from . import main2
 
 __project_name__ = "itsport"
 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dir", type=str, required=True, help="Directory containing PNG files"
+    )
+    return parser.parse_args()
+
+
 def main() -> int:
-    out = main2.render_template("extended.j2")
+    args = parse_arguments()
+    out = main2.render_template("extended.j2", data=args)
     sys.stdout.write(out)
     return 0
--- src/itsport/main2.py
+++ src/itsport/main2.py
@@ -1,5 +1,7 @@
+import os
 import pathlib
 
+import exifread
 import jinja2
 
 
@@ -12,4 +14,31 @@ def get_template(template_name):
 
 def render_template(template_name, data=None):
     template = get_template(template_name)
+
+    if data:
+        dir_path = data.dir
+        exif_data = []
+        for filename in os.listdir(dir_path):
+            if filename.endswith(".png"):
+                file_path = os.path.join(dir_path, filename)
+
+                with open(file_path, "rb") as f:
+                    tags = exifread.process_file(f)
+
+                    file_data = {"filename": filename, "exif_tags": []}
+                    for tag in tags.keys():
+                        if tag not in (
+                            "JPEGThumbnail",
+                            "TIFFThumbnail",
+                            "Filename",
+                            "EXIF MakerNote",
+                        ):
+                            file_data["exif_tags"].append(
+                                {"name": tag, "value": str(tags[tag])}
+                            )
+
+                    exif_data.append(file_data)
+
+        data.exif_data = exif_data
+
     return template.render(data=data)
